f052557fbb3d4d97fdb4816d1be3fe9bc7d28f8a
Camille Goudeseune <cog@illinois.edu>
2013-04-08 15:57:43 -0500

    Paris's sndr-ui, builds/runs on ubuntu, despite no aquaterm

---
+++ sndr-ui/aufeat.cpp
@@ -0,0 +1,59 @@
+#include "wavfile.h"
+#include "aufeat.h"
+#include "optparse.h"
+
+#include <iostream>
+
+#include "aquat.h"
+
+using namespace std;
+
+typedef double real_t;
+
+int main( int argc, const char **argv)
+{
+	// Analysis options
+	const string fopts = getoption<string>( "-F", argc, argv, string( "cdm"), "Feature options"); // Feature options string
+	const int sz = getoption<int>( "-s", argc, argv, 1024, "FFT size"); // Size of underlying FFT
+	const int b = getoption<int>( "-n", argc, argv, 13, "Feature coefficients"); // Number of coeffs to use
+#ifdef UNUSED
+	const real_t flo = getoption<real_t>( "-l", argc, argv, 80, "Lowest frequency"); // Lowest frequency
+	const real_t fhi = getoption<real_t>( "-h", argc, argv, 7500, "Highest frequency"); // Highest frequency
+#endif
+	const int fb = getoption<int>( "-b", argc, argv, 32, "Filterbank filters"); // Filterbank filters
+
+	const string infile = getoption<string>( "-i", argc, argv, string( "80s.wav"), "Input soundfile"); // Input soundfile(s)
+
+	// Open the soundfile
+	wavfile_t w( infile);
+	const real_t sr = w.samplerate;
+
+	// Allocate features object
+	aufeat_t<real_t> f;
+	f.setup( sr, sz, fb, b, 50, sr/2, fopts);
+
+	// Open a graphics terminal
+	aq_window( 1, "Features");
+
+	// Process
+	array<real_t> x( 1024);
+	array<real_t> y( 1024);
+	array<real_t> Y;
+	for(int i=0; w.file; ++i){
+		cout << "." << endl;
+		// Read a buffer
+		w.read_mono( x);
+
+		// Get its features
+		f.extract( x, y);
+
+		// Store them inside a matrix
+		if( !Y.size())
+			Y.resize( y.size(), w.frames/sz+1);
+		for( size_t j = 0 ; j < y.size() ; ++j)
+			Y(j,i) = pow( y(j), 0.3);
+	}
+
+	// Show me
+	aq_image( Y.v, Y.m, Y.n);
+}
---
+++ sndr-ui/optparse.h
@@ -0,0 +1,76 @@
+// optparse.h -- Functions to parse option string
+// language: C++
+// author  : Paris Smaragdis
+// copyright: (c) 2009 Adobe Systems Incorporated, All Rights Reserved
+
+#ifndef __OPTPARSE_H
+#define __OPTPARSE_H
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <typeinfo> // for typeid() with gcc 4.4+
+
+//
+// Option parsing routine
+//
+
+template <class T> T optassign( const char *o) { std::cout << typeid( T).name() << std::endl; throw std::runtime_error( "Unsupported data type in optassign"); }
+template <> int optassign<int>( const char *o) { return atoi( o); }
+template <> double optassign<double>( const char *o) { return atof( o); }
+template <> std::string optassign<std::string>( const char *o) { return std::string( o); }
+
+template <class T>
+T getoption( std::string opt, int argc, const char **argv, T iv = T(), const char *descr = NULL)
+{
+	T v = iv;
+	for( int i = 0 ; i < argc ; i++){
+		// Find option flag
+		if( !strncmp( argv[i], opt.c_str(), opt.size())){
+
+			// Assign accordingly
+			if( strcmp( argv[i], opt.c_str()) == 0)
+				v = optassign<T>( argv[i+1]);
+			else
+				v = optassign<T>( argv[i]+opt.size());
+
+			if( descr)
+				std::cout << descr << " is " << v << std::endl;
+			else
+				std::cout << "Flag " << opt << " is " << v << std::endl;
+		}
+	}
+	return v;
+}
+
+template <class T>
+array<T> mgetoption( std::string opt, int argc, const char **argv, const char *descr = NULL)
+{
+	array<T> v;
+	for( int i = 0 ; i < argc ; i++){
+		// Find option flag
+		if( !strncmp( argv[i], opt.c_str(), opt.size())){
+			
+			// Assign accordingly
+			if( strcmp( argv[i], opt.c_str()) != 0)
+				v.push_back( optassign<T>( argv[i]+opt.size()));
+			for( int j = i+1 ; j < argc & argv[j][0] != '-' ; j++)
+				v.push_back( optassign<T>( argv[j]));
+
+			if( descr)
+				std::cout << descr << " is ";
+			else
+				std::cout << "Flag " << opt << " is ";
+			for( int j = 0 ; j < v.size() ; j++)
+				std::cout << v(j) << ", ";
+			std::cout << std::endl;
+		}
+	}
+
+	// By default leave empty
+	return v;
+}
+
+#endif
