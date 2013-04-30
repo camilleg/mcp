501505bbb178e3fa5136c6171751b217129cc28f
Camille Goudeseune <cog@illinois.edu>
2013-04-30 13:11:18 -0500

    fewer simplifying assumptions for ffmpeg

--- sndr-ui/aufeat.cpp
+++ sndr-ui/aufeat.cpp
@@ -6,66 +6,65 @@
 extern int ffmpeg_init();
 extern bool ffmpeg_open(const std::string& filename);
 extern bool ffmpeg_eof();
-extern bool ffmpeg_read( array<double> &p);
+extern bool ffmpeg_read(array<double> &p);
 extern double ffmpeg_samplerate();
 
-int main( int argc, const char **argv)
+int main(int argc, const char **argv)
 {
 	typedef double real_t;
 
 	// Analysis options
-	const std::string fopts = getoption<std::string>( "-F", argc, argv, "cdm", "Feature options"); // e.g., a1w26c14dD
-	const int fft_size = 2304;;;; // getoption<int>( "-s", argc, argv, 1024, "FFT size"); // e.g., 512
-	const int b = getoption<int>( "-n", argc, argv, 13, "Feature coefficients"); // Number of coeffs
+	const std::string fopts = getoption<std::string>("-F", argc, argv, "cdm", "Feature options"); // e.g., a1w26c14dD
+	const int fft_size = 1024;;;; // getoption<int>("-s", argc, argv, 1024, "FFT size"); // e.g., 512
+	const int b = getoption<int>("-n", argc, argv, 13, "Feature coefficients"); // Number of coeffs
 #ifdef UNUSED
-	const real_t flo = getoption<real_t>( "-l", argc, argv, 80, "Lowest frequency"); // in Hz
-	const real_t fhi = getoption<real_t>( "-h", argc, argv, 7500, "Highest frequency"); // in Hz
+	const real_t flo = getoption<real_t>("-l", argc, argv, 80, "Lowest frequency"); // in Hz
+	const real_t fhi = getoption<real_t>("-h", argc, argv, 7500, "Highest frequency"); // in Hz
 #endif
-	const int fb = getoption<int>( "-b", argc, argv, 32, "Filterbank filters");
+	const int fb = getoption<int>("-b", argc, argv, 32, "Filterbank filters");
 
-	const std::string infileName = getoption<std::string>( "-i", argc, argv, "/dev/null", "Input soundfile");
+	const std::string infileName = getoption<std::string>("-i", argc, argv, "/dev/null", "Input soundfile");
 
 	if (ffmpeg_init() != 0)
 	  return 1;
 
-	// Open the soundfile
+	// Open soundfile.
 	if (!ffmpeg_open(infileName))
 	  return 1;
 	const real_t sr = ffmpeg_samplerate();
 
-	// Allocate features object
+	// Allocate feature.
 	aufeat_t<real_t> feature;
-	feature.setup( sr, fft_size, fb, b, 50, sr/2, fopts);
+	feature.setup(sr, fft_size, fb, b, 50, sr/2, fopts);
 
-	// Open graphics terminal
-	aq_window( 1, "Features");
+	// Open graphics terminal.
+	aq_window(1, "Features");
 
-	// Process
 	array<real_t> x(fft_size);
 	array<real_t> y(fft_size);
 	array<real_t> Y;
-	for (int i=0; !ffmpeg_eof(); ++i){
-		// Read a buffer.
-		if (!ffmpeg_read( x)) {
+	for (int i=0; !ffmpeg_eof(); ++i) {
+		// Read buffer x from soundfile.
+		if (!ffmpeg_read(x)) {
 		  continue;
 		}
 
-		// Get buffer's features.
-		feature.extract( y, x);
+		// Compute feature y from buffer x.
+		feature.extract(y, x);
 
-		// Store features in matrix.
-		if( Y.empty()) {
-#define hack 5000
+		// Accumulate feature y into matrix Y.
+		if (Y.empty()) {
+#define hack 15000
 			printf("resizing to %lu x %d\n", y.size(), hack);
-			Y.resize( y.size(), hack); // fft_size/*wavfile.h's infile.frames*/ /fft_size+1;
+			Y.resize(y.size(), hack); // fft_size/*wavfile.h's infile.frames*/ /fft_size+1;
 		}
 		printf("stuffing Y(%d .. %lu, %d)\n", 0, y.size()-1, i);
-		for( size_t j = 0; j < y.size(); ++j)
-			Y(j,i) = pow( y(j), 0.3);
+		for (size_t j = 0; j < y.size(); ++j)
+			Y(j,i) = pow(y(j), 0.3);
 	}
 	std::cerr << std::endl;
 
-	// Show feature
-	aq_image( Y.v, Y.m, Y.n);
+	// Show feature.
+	aq_image(Y.v, Y.m, Y.n);
 	return 0;
 }
--- sndr-ui/auffmpeg.cpp
+++ sndr-ui/auffmpeg.cpp
@@ -4,13 +4,15 @@
 #define UINT64_C(c) (c ## ULL)
 #endif
 
-#include "array.h"
-
 extern "C" {
 #include <libavcodec/avcodec.h>		// apt-get install libavcodec-dev
 #include <libavformat/avformat.h>	// apt-get install libavformat-dev
 }
 
+#include <cassert>
+#include "array.h"
+
+// todo: encapsulate into a class all globals and statics in this .cpp file
 AVFormatContext *pFormatContext = NULL;
 AVCodecContext *pDecoderContext = NULL;
 int iAudioStream = -1;
@@ -60,76 +62,119 @@ int ffmpeg_init()
 }
 
 int ret_opened = 0;
+bool hit_eof = false;
 
 bool ffmpeg_open(const std::string& filename)
 {
-    return (ret_opened = open_input_file(filename.c_str())) >= 0;
+  hit_eof = false;
+  return (ret_opened = open_input_file(filename.c_str())) >= 0;
 }
 
 void debug_frame(const AVFrame *frame)
 {
-    const int n = frame->nb_samples * av_get_channel_layout_nb_channels(av_frame_get_channel_layout(frame));
-    static int a = 0; a += n;
-    int64_t dur = av_frame_get_pkt_duration(frame); // in AVStream->time_base units
-    double seconds = (dur * timebase.num) / double(timebase.den);
-    static double s = 0; s += seconds;
-    printf("another %d samples, %f seconds, respective totals %d %f\n", n, seconds, a, s);
+  const int n = frame->nb_samples * av_get_channel_layout_nb_channels(av_frame_get_channel_layout(frame));
+  static int a = 0; a += n;
+  int64_t dur = av_frame_get_pkt_duration(frame); // in AVStream->time_base units
+  double seconds = (dur * timebase.num) / double(timebase.den);
+  static double s = 0; s += seconds;
+  printf("another %d samples, %f seconds, respective totals %d %f\n", n, seconds, a, s);
 }
-
-bool hit_eof = false;
 
 bool ffmpeg_eof() { return hit_eof; }
 
-double ffmpeg_samplerate() { return 44100.0;;;; }
+double ffmpeg_samplerate() { return pDecoderContext->sample_rate; }
+
+bool ffmpeg_read_frame(uint16_t*& pw, size_t& cw)
+{
+  if (hit_eof || ret_opened < 0)
+      return false;
+  int ret;
+  AVPacket packet;
+again:
+  do {
+    ret = av_read_frame(pFormatContext, &packet);
+    if (ret < 0) {
+      hit_eof = true;
+      if (pDecoderContext)
+	avcodec_close(pDecoderContext);
+      avformat_close_input(&pFormatContext);
+      if (ret < 0 && ret != AVERROR_EOF) {
+	char buf[1024];
+	av_strerror(ret, buf, sizeof(buf));
+	fprintf(stderr, "Error occurred: %s\n", buf);
+      }
+      return false;
+    }
+  } while (packet.stream_index != iAudioStream);
+
+  avcodec_get_frame_defaults(src_frame);
+  int got_frame = 0;
+  ret = avcodec_decode_audio4(pDecoderContext, src_frame, &got_frame, &packet);
+  if (ret < 0) {
+    av_log(NULL, AV_LOG_ERROR, "Error decoding audio\n");
+    return false;
+  }
+
+  if (got_frame) {
+    // debug_frame(src_frame);
+    const size_t csamples = src_frame->nb_samples *
+      av_get_channel_layout_nb_channels(av_frame_get_channel_layout(src_frame));
+    if (csamples == 0) {
+      printf("Skipping frame with 0 samples.\n");;;;
+      goto again;
+    }
+    cw = csamples;
+    static uint16_t buf[100000]; //;;;; hardcoded length
+    const uint16_t* pSrc = (const uint16_t*)src_frame->data[0];
+    std::copy(pSrc, pSrc+cw, buf); //;;;; extra array-copy.  Nicer would be to av_free_packet "after return."
+    pw = buf;
+  }
+  av_free_packet(&packet);
+  return true;
+}
 
 // for .h file: template <class T> bool ffmpeg_read( array<T> &p)
 bool ffmpeg_read(array<double> &dst)
 {
-    if (hit_eof || ret_opened < 0)
-	return false;
-    int ret;
-    AVPacket packet;
-again:
-    do {
-	ret = av_read_frame(pFormatContext, &packet);
-	if (ret < 0) {
-	    hit_eof = true;
-	    if (pDecoderContext)
-		avcodec_close(pDecoderContext);
-	    avformat_close_input(&pFormatContext);
-	    if (ret < 0 && ret != AVERROR_EOF) {
-		char buf[1024];
-		av_strerror(ret, buf, sizeof(buf));
-		fprintf(stderr, "Error occurred: %s\n", buf);
-	    }
-	    return false;
-	}
-    } while (packet.stream_index != iAudioStream);
+  if (hit_eof || ret_opened < 0)
+    return false;
 
-    avcodec_get_frame_defaults(src_frame);
-    int got_frame = 0;
-    ret = avcodec_decode_audio4(pDecoderContext, src_frame, &got_frame, &packet);
-    if (ret < 0) {
-        av_log(NULL, AV_LOG_ERROR, "Error decoding audio\n");
-	return false;
+  uint16_t* pw = NULL;
+  size_t cw = 0;
+
+  static double cache[100000]; //;;;; hardcoded length.  ;;;;use an array<double> instead.
+  static size_t icache = 0;
+
+  const size_t cwDst = dst.size();
+  
+  // Drain cache into dst before reading more frames.
+  size_t i = std::min(cwDst, icache);
+  memcpy(&dst[0], cache, i*sizeof(cache[0])); // todo: std::copy(cache, cache+j, dst);
+
+  if (i < icache) {
+    // Reconstruct incompletely drained cache.
+    // todo: for speed, replace memmove'd array with fifo or circular buffer.
+    memmove(cache, cache+i, (icache-i) * sizeof(cache[0]));
+    icache -= i;
+  }
+
+  // Continue filling dst[i..cwDst].
+  while (i < cwDst) {
+    if (!ffmpeg_read_frame(pw, cw)) {
+      // Failed to read another frame from file.
+      return false;
     }
-    if (got_frame) {
-        // debug_frame(src_frame);
-	const size_t n = src_frame->nb_samples *
-	    av_get_channel_layout_nb_channels(av_frame_get_channel_layout(src_frame));
-	if (n == 0) {
-	  printf("Skipping frame with 0 samples.\n");;;;
-	  goto again;
-	}
-	const uint16_t *p = (uint16_t*)src_frame->data[0];
-	if (n != dst.size()) {
-	  printf("nyi: intermediate buffering.  %lu != %lu\n", n, dst.size());
-	  exit(1);
-	}
-	//printf("fyi, stuffing %lu samples.\n", n);;;;
-	for (size_t i=0; i<n; ++i)
-	  dst[i] = *p++;
+    // Fill dst[i..] from pw[0..cw].
+    size_t iw = std::min(cwDst-i, cw);
+    memcpy(&dst[i], pw, iw*sizeof(pw[0]));
+    i += iw;
+    if (iw < cw) {
+      assert(i == cwDst); // Filled dst.
+      // Save the unused samples pw[iw..cw] into local cache.
+      memcpy(cache+icache, pw+iw, (cw-iw)*sizeof(pw[0]));
+      icache += cw-iw;
+      assert(icache < sizeof(cache)/sizeof(cache[0])); // hardcoded length
     }
-    av_free_packet(&packet);
-    return true;
+  }
+  return true;
 }
