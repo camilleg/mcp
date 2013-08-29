8e315140a0229e89b25e7037cc4c6ef53c6fad4a
Camille Goudeseune <cog@illinois.edu>
2013-08-29 14:59:40 -0500

    scripts: better training flags

--- sndr-tools/homer-sndr.sh
+++ sndr-tools/homer-sndr.sh
@@ -8,12 +8,12 @@ bin=bin
 snd=../simpsons
 
 # Parameters
-set train    = "-t .25 -H 8 -T .1 -l 60 -K 20 -e 500"
-set classify = "-p .9999 -f 2"
+train="-t .25 -H 8 -T .4 -l 45 -h 4500 -K 7 -e 120"
+classify="-p .9999 -f 2"
 
 echo "Learn model."
 $bin/sndr $train -i $snd/simp.wav -g $snd/homer.wav -M models/homer
 
 echo
-echo "Find Homer and not-Homer (into simp.wav.homer.?.wav)."
+echo "Find Homer and not-Homer (into simp.wav.homer.*.wav)."
 $bin/sndr $train $classify -i $snd/simp.wav -m models/homer-target models/homer-ubm -M models/homer-combined -d homer -D homer
--- sndr-tools/homer-track.sh
+++ sndr-tools/homer-track.sh
@@ -7,8 +7,8 @@ bin=bin
 snd=../simpsons
 
 # Parameters
-set train    = "-K 8"
-set classify = "-p .9999"
+train="-K 8"
+classify="-p .9999"
 
 echo "Learn and find Homer."
 $bin/snd_track -i $snd/simp.wav -t $snd/homer.wav -f $snd/homer-track.wav $train $classify
