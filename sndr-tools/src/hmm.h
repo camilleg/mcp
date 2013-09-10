0281a2fd9b3bfb6904175d9093f7f45f3d4f7d58
Camille Goudeseune <cog@illinois.edu>
2013-09-10 09:39:38 -0500

    for loops

--- sndr-tools/src/hmm.h
+++ sndr-tools/src/hmm.h
@@ -72,11 +72,11 @@ public:
 		if( H.S == 0){
 
 			// Initial values for Gaussians
-			for( int s = 0; s < S; ++s){
-				for( int k = 0; k < K; ++k){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
 					ldt(k,s) = 0;
 					c  (k,s) = 1./K;
-					for( int i = 0; i < M; ++i){
+					for( int i=0; i<M; ++i){
 						m (i,k,s) = T( rand())/RAND_MAX - 0.5;
 						is(i,k,s) = 0.1;
 						ldt(k,s) += log( is(i,k,s));
@@ -85,26 +85,26 @@ public:
 			}
 
 			// Initial values for initial and transition probabilities
-			for( int s = 0; s < S; ++s)
+			for( int s=0; s<S; ++s)
 				lPi(s) = log( 1./S);
-			for( int i = 0; i < S; ++i)
-				for( int j = 0; j < S; ++j)
+			for( int i=0; i<S; ++i)
+				for( int j=0; j<S; ++j)
 					lA(i,j) = log( T(rand())/RAND_MAX);
-			for( int i = 0; i < S; ++i){
+			for( int i=0; i<S; ++i){
 				T ls = log(0.0);
-				for( int j = 0; j < S; ++j)
+				for( int j=0; j<S; ++j)
 					logadd( ls, lA(i,j));
-				for( int j = 0; j < S; ++j)
+				for( int j=0; j<S; ++j)
 					lA(i,j) -= ls;
 			}
 		}else{
 			
 			// Copy values of Gaussians
-			for( int s = 0; s < S; ++s){
-				for( int k = 0; k < K; ++k){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
 					ldt(k,s) = H.ldt(k,s);
 					c(k,s) = H.c(k,s);
-					for( int i = 0; i < M; ++i){
+					for( int i=0; i<M; ++i){
 						m (i,k,s) = H.m (i,k,s);
 						is(i,k,s) = H.is(i,k,s);
 					}
@@ -112,9 +112,9 @@ public:
 			}
 
 			// Copy values of initial and transition probabilities
-			for( int i = 0; i < S; ++i){
+			for( int i=0; i<S; ++i){
 				lPi(i) = H.lPi(i);
-				for( int j = 0; j < S; ++j)
+				for( int j=0; j<S; ++j)
 					lA(i,j) = H.lA(i,j);
 			}
 		}
@@ -130,17 +130,17 @@ public:
 		array<T> lk( iters);
 
 		// Iterate expectation-maximization
-		for( int it = 0; it < iters; ++it){
+		for( int it=0; it<iters; ++it){
 		
 			// *** Expectation step ***
 
 			// Get likelihoods from each gaussian from each state
-			for( int s = 0 ; s < S ; s++){
-				for( int k = 0 ; k < K ; k++){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
 					T gc = log( c(k,s)) + 0.5*ldt(k,s) - 0.5*M*log(2*M_PI);
-					for( int j = 0 ; j < N ; j++){
+					for( int j=0; j<N; ++j){
 						T qt = 0;
-						for( int i = 0 ; i < M ; i++)
+						for( int i=0; i<M; ++i)
 							qt += is(i,k,s) * (x(j,i) - m(i,k,s)) * (x(j,i) - m(i,k,s));
 						q(j,k,s) = gc - 0.5*qt;
 					}
@@ -148,119 +148,120 @@ public:
 			}
 
 			// Get overall state likelihoods
-			for( int s = 0 ; s < S ; s++)
-				for( int j = 0 ; j < N ; j++){
+			for( int s=0; s<S; ++s)
+				for( int j=0; j<N; ++j){
 					T tp = log( 0.0);
-					for( int k = 0 ; k < K ; k++)
+					for( int k=0; k<K; ++k)
 						logadd( tp, q(j,k,s));
 					lp(s,j) = tp;
 				}
 
 			// Get alphas
-			for( int i = 0 ; i < S ; i++)
+			for( int i=0; i<S; ++i)
 				la(i,0) = lPi(i) + lp(i,0);
-			for( int t = 0 ; t < N-1 ; t++)
-				for( int j = 0 ; j < S ; j++){
+			for( int t=0; t<N-1; ++t)
+				for( int j=0; j<S; ++j){
 					T ls = log( 0.0);
-					for( int i = 0 ; i < S ; i++)
+					for( int i=0; i<S; ++i)
 						logadd( ls, la(i,t) + lA(i,j));
 					la(j,t+1) = ls + lp(j,t+1);
 				}
 
 			// Get betas
-			for( int i = 0 ; i < S ; i++)
+			for( int i=0; i<S; ++i)
 				lb(i,N-1) = log( 0.0);
 			lb(S-1,N-1) = 0;
 			for( int t = N-2 ; t > -1 ; t--)
-				for( int i = 0 ; i < S ; i++){
+				for( int i=0; i<S; ++i){
 					T ls = log( 0.0);
-					for( int j = 0 ; j < S ; j++)
+					for( int j=0; j<S; ++j)
 						logadd( ls, lb(j,t+1) + lA(i,j) + lp(j,t+1));
 					lb(i,t) = ls;
 				}
 
 			// Get Xi
-			for( int i = 0 ; i < S*S ; i++)
+			for( int i=0; i<S*S; ++i)
 				xi(i) = log( 0.0);
-			for( int t = 0 ; t < N-1 ; t++){
+			for( int t=0; t<N-1; ++t){
 				T ls = log( 0.0);
-				for( int i = 0 ; i < S ; i++)
-					for( int j = 0 ; j < S ; j++){
+				for( int i=0; i<S; ++i)
+					for( int j=0; j<S; ++j){
 						txi(i,j) = lA(i,j) + la(i,t) + lp(j,t+1) + lb(j,t+1);
 						logadd( ls, txi(i,j));
 					}
-				for( int i = 0 ; i < S*S ; i++)
+				for( int i=0; i<S*S; ++i)
 					logadd( xi(i), txi(i) - ls);
 			}
 
 			// Get gamma
-			for( int j = 0 ; j < N ; j++){
+			for( int j=0; j<N; ++j){
 				T ls = log( 0.0);
-				for( int s = 0 ; s < S ; s++)
+				for( int s=0; s<S; ++s)
 					logadd( ls, la(s,j)+lb(s,j));
-				for( int s = 0 ; s < S ; s++){
+				for( int s=0; s<S; ++s){
 					T tg = la(s,j) + lb(s,j) - lp(s,j) - ls;
-					for( int k = 0 ; k < K ; k++)
+					for( int k=0; k<K; ++k)
 						g(j,k,s) = tg + q(j,k,s);
 				}
 			}
 
 			// Get overall likelihood
 			lk(it) = log( 0.0);
-			for( int i = 0 ; i < S ; i++) {
+			for( int i=0; i<S; ++i) {
 				logadd( lk(it), la(i,N-1));
 			}
-			if( 1){ //( !((it+1)%25) | it == iters-1){
+			if( ((it+1)%5==0) || it == iters-1){
 				std::cout << "HMM iteration " << it+1 << " of " << iters << ": likelihood " << lk(it) << std::endl;
 			}
 
 			// Get out of log domain
-			for( int i = 0 ; i < N*K*S ; i++)
+			for( int i=0; i<N*K*S; ++i)
 				g(i) = exp( g(i));
+
 
 			// *** Maximization step ***
 
 			// Initial probabilities
-			for( int s = 0 ; s < S ; s++){
+			for( int s=0; s<S; ++s){
 				T tp = log( 0.0);
-				for( int i = 0 ; i < K ; i++)
+				for( int i=0; i<K; ++i)
 					logadd( tp, log( g(0,i,s)));
 				lPi(s) = tp;
 			}
 
 			// Transition matrix
-			for( int i = 0 ; i < S ; i++){
+			for( int i=0; i<S; ++i){
 				T ls = log( 0.0);
-				for( int j = 0 ; j < S ; j++)
+				for( int j=0; j<S; ++j)
 					logadd( ls, xi(i,j));
-				for( int j = 0 ; j < S ; j++)
+				for( int j=0; j<S; ++j)
 					lA(i,j) = xi(i,j) - ls;
 			}
 
 			// Priors
-			for( int s = 0 ; s < S ; s++){
-				for( int k = 0 ; k < K ; k++){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
 					T tc = 0;
-					for( int i = 0 ; i < N ; i++)
+					for( int i=0; i<N; ++i)
 						tc += g(i,k,s);
 					c(k,s) = tc;
 				}
 			}
-			for( int s = 0 ; s < S ; s++){
+			for( int s=0; s<S; ++s){
 				T sc = 0;
-				for( int k = 0 ; k < K ; k++)
+				for( int k=0; k<K; ++k)
 					sc += c(k,s);
-				for( int k = 0 ; k < K ; k++)
+				for( int k=0; k<K; ++k)
 					c(k,s) /= sc;
 			}
 			// ******* IS SCALING RIGHT?  I get c = [1 1 1 1 1 ...]
 
 			// Means
-			for( int s = 0 ; s < S ; s++){
-				for( int k = 0 ; k < K ; k++){
-					for( int i = 0 ; i < M ; i++){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
+					for( int i=0; i<M; ++i){
 						T ms = 0, sg = 0;
-						for( int j = 0 ; j < N ; j++){
+						for( int j=0; j<N; ++j){
 							ms += x(j,i) * g(j,k,s);
 							sg += g(j,k,s);
 						}
@@ -270,12 +271,12 @@ public:
 			}
 
 			// Covariances
-			for( int s = 0 ; s < S ; s++){
-				for( int k = 0 ; k < K ; k++){
+			for( int s=0; s<S; ++s){
+				for( int k=0; k<K; ++k){
 					ldt(k,s) = 0;
-					for( int i = 0 ; i < M ; i++){
+					for( int i=0; i<M; ++i){
 						T tu = 0, sg = 0;
-						for( int j = 0 ; j < N ; j++){
+						for( int j=0; j<N; ++j){
 							tu += (x(j,i)-m(i,k,s))*(x(j,i)-m(i,k,s))*g(j,k,s);
 							sg += g(j,k,s);
 						}
@@ -295,16 +296,16 @@ public:
 
 		// Get state probabilities
 		array<T> lB( S, N);
-		for( int i = 0 ; i < S*N ; ++i)
+		for( int i=0; i<S*N; ++i)
 			lB(i) = log(0.0); // Init all lB(s,j)'s.
 
 #pragma omp parallel for
-		for( int s = 0 ; s < S ; s++)
-			for( int k = 0 ; k < K ; k++){
+		for( int s=0; s<S; ++s)
+			for( int k=0; k<K; ++k){
 				const T gc = log( c(k,s)) + 0.5*ldt(k,s) - 0.5*M*log(2*M_PI);
-				for( int j = 0 ; j < N ; j++){
+				for( int j=0; j<N; ++j){
 					T qt = 0;
-					for( int i = 0 ; i < M ; i++)
+					for( int i=0; i<M; ++i)
 						qt += is(i,k,s) * (x(j,i) - m(i,k,s)) * (x(j,i) - m(i,k,s));
 					logadd( lB(s,j), gc - 0.5*qt);
 				}
@@ -314,8 +315,8 @@ public:
 		if( bias.size()){
 			if( int(bias.size()) != S)
 				throw std::runtime_error( "clasify(): Bias vector has wrong size");
-			for( int i = 0 ; i < S ; i++)
-				for( int j = 0 ; j < N ; j++)
+			for( int i=0; i<S; ++i)
+				for( int j=0; j<N; ++j)
 					lB(i,j) += bias(i);
 		}
 
@@ -335,28 +336,28 @@ public:
 		// Override initial state if one is provided
 		array<double> nlPi(S);
 		if( ist != -1){
-			for( int i = 0 ; i < S ; i++)
+			for( int i=0; i<S; ++i)
 				nlPi(i) = log( 0.0);
 			nlPi(ist) = 0.0;
 		}else
-			for( int i = 0 ; i < S ; i++)
+			for( int i=0; i<S; ++i)
 				nlPi(i) = lPi(i);
 
 		// Initialize
 		int di = 0;
-		for( int i = 0 ; i < S ; i++){
+		for( int i=0; i<S; ++i){
 			d(i,di) = nlPi(i) + lB(i,0);
 			p(i,0) = 0;
 		}
 
 		// Propagate
-		for( int t = 1 ; t < N ; t++){
-			for( int i = 0 ; i < S ; i++){
+		for( int t=1; t<N; ++t){
+			for( int i=0; i<S; ++i){
 
 				// Find max likelihood and its index on temp variables
 				T dt = -HUGE_VAL;
 				int pt = 0;
-				for( int j = 0 ; j < S ; j++){
+				for( int j=0; j<S; ++j){
 					T tt = lA(j,i) + d(j,di);
 					if( tt > dt){
 						dt = tt;
@@ -377,7 +378,7 @@ public:
 		// Terminate
 		T l = d(0,di);
 		q(N-1) = 0;
-		for( int i = 1 ; i < S ; i++)
+		for( int i=1; i<S; ++i)
 			if( d(i,di) > l){
 				l = d(i,di);
 				q(N-1) = i;
@@ -399,7 +400,7 @@ public:
 
 		// Initial probability vector
 		array<T> pp( S);
-		for( int i = 0 ; i < S ; i++)
+		for( int i=0; i<S; ++i)
 			pp(i) = lPi(i);
 
 		int a = 0;
@@ -411,9 +412,9 @@ public:
 
 			// Find fusion point (where all possible solutions converge to the same state)
 			int t = 0;
-			for( int j = 0 ; j < s.n ; j++){
+			for( int j=0; j<s.n; ++j){
 				bool e = true;
-				for( int i = 0 ; i < S-1 ; i++)
+				for( int i=0; i<S-1; ++i)
 					e &= s(i,j) == s(i+1,j);
 
 				if( !e){
@@ -424,9 +425,9 @@ public:
 
 			if( t > 0){
 				// Keep advance
-				for( int i = 0 ; i < t ; i++)
+				for( int i=0; i<t; ++i)
 					q(a+i) = s(0,i);
-				for( int i = 0 ; i < S ; i++)
+				for( int i=0; i<S; ++i)
 					pp(i) = lA(q(a+t-1),i);
 				a += t;
 				b = a+1;
@@ -448,19 +449,19 @@ public:
 		
 		// Initialize
 		int di = 0;
-		for( int i = 0 ; i < S ; i++){
+		for( int i=0; i<S; ++i){
 			d(i,di) = lP(i) + lB(i,a);
 			p(i,0) = 0;
 		}
 
 		// Propagate
-		for( int t = 1 ; t < N ; t++){
-			for( int i = 0 ; i < S ; i++){
+		for( int t=1; t<N; ++t){
+			for( int i=0; i<S; ++i){
 				
 				// Find max likelihood and its index on temp variables
 				T dt = -HUGE_VAL;
 				int pt = 0;
-				for( int j = 0 ; j < S ; j++){
+				for( int j=0; j<S; ++j){
 					T tt = lA(j,i) + d(j,di);
 					if( tt > dt){
 						dt = tt;
@@ -479,7 +480,7 @@ public:
 		q.resize( S, N);
 
 		// Try all possible terminations
-		for( int i = 0 ; i < S ; i++){
+		for( int i=0; i<S; ++i){
 			q(i,N-1) = i;
 			
 			// Backtrack
@@ -589,10 +590,10 @@ public:
 
 		// Compute the determinants.
 		ldt.resize( K, S);
-		for( int s = 0 ; s < S ; ++s)
-			for( int k = 0 ; k < K ; ++k){
+		for( int s=0; s<S; ++s)
+			for( int k=0; k<K; ++k){
 				ldt(k,s) = 0;
-				for( int i = 0 ; i < M ; ++i)
+				for( int i=0; i<M; ++i)
 					ldt(k,s) += log( is(i,k,s));
 			}
 	}
@@ -621,36 +622,36 @@ void combine( hmm_t<T> &H, const hmm_t<T> &h1, const hmm_t<T> &h2, T p1 = 0.5, T
 	H.is .resize( M, H.K, H.S);
 
 	// Copy data.
-	for( int s = 0; s < h1.S; ++s)
-		for( int k = 0; k < h1.K; ++k){
+	for( int s=0; s<h1.S; ++s)
+		for( int k=0; k<h1.K; ++k){
 			H.c  (k,s) = h1.c  (k,s);
 			H.ldt(k,s) = h1.ldt(k,s);
-			for( size_t i = 0; i < M; ++i){
+			for( size_t i=0; i<M; ++i){
 				H.m (i,k,s) = h1.m (i,k,s);
 				H.is(i,k,s) = h1.is(i,k,s);
 			}
 		}
-	for( int s = 0; s < h2.S; ++s)
-		for( int k = 0; k < h2.K; ++k){
+	for( int s=0; s<h2.S; ++s)
+		for( int k=0; k<h2.K; ++k){
 			H.c  (k,h1.S+s) = h2.c  (k,s);
 			H.ldt(k,h1.S+s) = h2.ldt(k,s);
-			for( size_t i = 0; i < M; ++i){
+			for( size_t i=0; i<M; ++i){
 				H.m (i,k,h1.S+s) = h2.m (i,k,s);
 				H.is(i,k,h1.S+s) = h2.is(i,k,s);
 			}
 		}
 
 	// Make transition matrix and initial probabilities.
-	for( size_t i = 0; i < H.lA.size(); ++i)
+	for( size_t i=0; i < H.lA.size(); ++i)
 		H.lA(i) = -HUGE_VAL;
-	for( int i = 0; i < h1.S; ++i){
+	for( int i=0; i < h1.S; ++i){
 		H.lPi(i) = h1.lPi(i);
-		for( int j = 0; j < h1.S; ++j)
+		for( int j=0; j < h1.S; ++j)
 			H.lA(i,j) = h1.lA(i,j);
 	}
-	for( int i = 0; i < h2.S; ++i){
+	for( int i=0; i < h2.S; ++i){
 		H.lPi(h1.S+i) = h2.lPi(i);
-		for( int j = 0; j < h2.S; ++j)
+		for( int j=0; j < h2.S; ++j)
 			H.lA(h1.S+i,h1.S+j) = h2.lA(i,j);
 	}
 	H.lA(h1.S-1,      h1.S) = log( p1);
@@ -658,14 +659,14 @@ void combine( hmm_t<T> &H, const hmm_t<T> &h1, const hmm_t<T> &h2, T p1 = 0.5, T
 
 	// Normalize them.
 	T ps = 0;
-	for( int i = 0; i < H.S; ++i)
+	for( int i=0; i<H.S; ++i)
 		ps += exp( H.lPi(i));
-	for( int i = 0; i < H.S; ++i)
+	for( int i=0; i<H.S; ++i)
 		H.lPi(i) = log( exp( H.lPi(i))/ps);
 
-	for( int i = 0; i < h1.S; ++i)
+	for( int i=0; i<h1.S; ++i)
 		H.lA(h1.S-1,i) += log( (1.-p1));		// why not log1p(-p1) ?
-	for( int i = 0; i < h2.S; ++i)
+	for( int i=0; i<h2.S; ++i)
 		H.lA(h1.S+h2.S-1,h1.S+i) += log( (1.-p2));	// why not log1p(-p2) ?
 }
 
